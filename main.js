a4646b20d5c9095dcc0f718a20f9130fb7557113
Shanay-Mehta <f20211322@goa.bits-pilani.ac.in>
2022-09-23 11:00:21 +0530

    Added basic styles and NASA logo

--- main.js
+++ main.js
@@ -1,57 +1,62 @@
-let imgHding=document.querySelector("h1.imgHding")
-let imgExplntn=document.querySelector("h4.imgExplntn");
-let date=document.querySelector("h3.date");
-let previous=document.querySelector(".previous")
-let next=document.querySelector("button.next")
-let image=document.querySelector("img");
-let myDate=new Date();
+let imgHding = document.querySelector("h1.imgHding");
+let imgExplntn = document.querySelector("h4.imgExplntn");
+let date = document.querySelector("h3.date");
+let previous = document.querySelector(".previous");
+let next = document.querySelector("button.next");
+let image = document.querySelector("img.img");
+let myDate = new Date();
 let crntDate;
 let month;
 let year;
-function dates(){
-crntDate=myDate.getUTCDate();
-month=(myDate.getUTCMonth())+1;
-year=myDate.getUTCFullYear();
-};
-let func=async()=>{
-  let response=await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${year}-${month}-${crntDate}`);
-  if(response.status!==200){
+function dates() {
+  crntDate = myDate.getUTCDate();
+  month = myDate.getUTCMonth() + 1;
+  year = myDate.getUTCFullYear();
+}
+let func = async () => {
+  let response = await fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${year}-${month}-${crntDate}`
+  );
+  if (response.status !== 200) {
     throw new Error("Cannot fetch data");
-  }
-  else{
-  let data=await response.json();
-  return data;
+  } else {
+    let data = await response.json();
+    return data;
   }
 };
-function display(){func().then((data)=>{
-  imgHding.innerText=data.title;
-  if(data.media_type==="image"){
-    image.src=data.url;
-    image.alt=data.title;
-  }
-  if(data.media_type==="video"){
-    image.src="";
-    image.alt="Response is a video!";
-  }
-  imgExplntn.innerText=data.explanation;
-  date.innerText=data.date;
-}).catch((err)=>{
-  imgHding.innerText=`Error: ${err.message}`;
-  imgExplntn.innerText="";
-  date.innerText="";
-  image.src="";
-  image.alt="";
-})}
-previous.addEventListener("click",()=>{
-  myDate.setDate((myDate.getDate())-1);
+function display() {
+  func()
+    .then((data) => {
+      imgHding.innerText = data.title;
+      if (data.media_type === "image") {
+        image.src = data.url;
+        image.alt = data.title;
+      }
+      if (data.media_type === "video") {
+        image.src = "";
+        image.alt = "Response is a video!";
+      }
+      imgExplntn.innerText = data.explanation;
+      date.innerText = data.date;
+    })
+    .catch((err) => {
+      imgHding.innerText = `Error: ${err.message}`;
+      imgExplntn.innerText = "";
+      date.innerText = "";
+      image.src = "";
+      image.alt = "";
+    });
+}
+previous.addEventListener("click", () => {
+  myDate.setDate(myDate.getDate() - 1);
   dates();
   display();
-})
-next.addEventListener("click",()=>{
-  myDate.setDate((myDate.getDate())+1);
+});
+next.addEventListener("click", () => {
+  myDate.setDate(myDate.getDate() + 1);
   dates();
   display();
-})
+});
 dates();
 display();
 // center everything
